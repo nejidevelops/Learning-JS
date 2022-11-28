c6840486dad7f7aab5b61a39fe855ef4bb4fa1c5
Newton Ombese <newtonombese1@gmail.com>
2022-11-28 08:03:02 +0300

    done with task 131

---
+++ task131/helper.js
@@ -0,0 +1,9 @@
+export const printArray = (arr) => {
+  // for (let i = 0; i < arr.length; i++) {
+  //   console.log(arr);
+  // }
+  for(var i = 0; i < arr.length ; i++){
+    console.log(arr[i]);
+  }
+  // console.log(arr.join() );
+}
---
+++ task131/solution.js
@@ -0,0 +1,5 @@
+import { printArray } from './helper.js';
+
+const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+
+printArray(months);
