344f16d0e2abb4ed5704ffcf4c439f8d12e5a2ab
Newton Ombese <newtonombese1@gmail.com>
2023-01-02 12:05:40 +0300

    worked on task 136

---
+++ task136/helper.js
@@ -0,0 +1,7 @@
+export const getTotal = (arr) => {
+  let total = 0
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i]
+  }
+  return total
+}
---
+++ task136/solution.js
@@ -0,0 +1,5 @@
+import { getTotal } from './helper.js';
+
+const revenue = [10, 10, 20, 55, 1, 5, 12, 15];
+
+console.log(`For the array ${revenue}, total is ${getTotal(revenue)}`);
